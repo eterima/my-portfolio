b95a379b6a7a4223655a91f681f72a0021b90b08
Eka <putu_eka_terima@yahoo.de>
2024-11-08 00:34:46 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,144 @@
+<script>
+  const profile = {
+    name: "Eka Terima",
+     title: "People & Culture Manager",
+     bio: "Hi, I'm Eka! I'm a People & Tech enthusiast working at epilot.",
+     skills: "As People & Tech enthusiast I combine passion for all things People, but also use of technology in my day to day work. Working in a fast paced software company I was able to proof my People & Culture skills from end-to-end: From employer branding, to recruiting (focus on tech), onboarding, people management, development and administration, to offboarding.",
+     philosophy: "Understanding the business, product, job roles and even having a high-level knowledge of the tech side helps me to bring the most value for the growth of our company. ",
+     cta: "Let's talk!",
+     email: "e.terima@epilot.cloud",
+     scheduleLink: "https://calendly.com/epilot-careers/coffee-eka",
+     portfolioLink: "https://www.epilot.cloud/en/company/careers",
+  };
+</script>
+
+<style>
+  
+  .container {
+    display:flex;
+     max-width: 800px;
+     margin: 0 auto;
+     padding-right: 2rem;
+     background-color: #f8f8f8;
+     color:white;
+     border-radius: 8px;
+     box-shadow: 0px 10px -14px 14px #FFF;
+  }
+  .profile-pic {
+     width: 300px;
+     height: 550px;
+     object-fit: cover;
+     margin-right: 2rem;
+  }
+  .content {
+     flex: 1;
+  }
+  h1 {
+     margin: 0,5;
+     text-align:left;
+     font-size: 1.8rem;
+     color: #222;
+  }
+  h2 {
+    text-align:left;
+    margin-top: 0.5rem;
+     font-size: 1.2rem;
+     color: #666;
+     font-weight: normal;
+  }
+  .bio {
+     margin: 1rem 0;
+     line-height: 1.5;
+     color:#000;
+     text-align:left;
+    
+  }
+  .skills {
+     margin-bottom: 1rem;
+     color: #000000;
+     text-align:left;
+  }
+  .contact-links {
+     color: #3b5f7f;
+     text-decoration: none;
+     font-size: 1.2rem;
+     text-align:left;
+  }
+
+  .contact-links a {
+   color: #010912; /* Set icon color */
+   margin-right: 20px;
+   font-size: 1.5rem; /* Adjust icon size */
+   text-decoration: none;
+}
+
+.contact-links a:last-child {
+   margin-right: 0; /* Remove margin on the last icon */
+}
+
+  .buttons {
+     margin-top: 1rem;
+     text-align:left;
+  }
+  .button {
+     padding: 0.5rem 1rem;
+     margin-right: 1rem;
+     background-color: #0a1c2d;
+     color: white;
+     border: none;
+     border-radius: 4px;
+     cursor: pointer;
+     text-align:left;
+     transition: background-color 0.3s, transform 0.3s, box-shadow 0.3s;
+
+  }
+
+.button:hover {
+   background-color: #d77015;
+   transform: scale(1.05);
+   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
+}
+
+
+  .philosophy {
+     text-align:left;
+     color: #000a13;
+  }
+
+  .cta {
+     text-align:left;
+     color: #000a13;
+  }
+</style>
+
+<div class="container">
+  <img src="/src/profile-pic.png" alt="Profile Picture" class="profile-pic" />
+
+  <div class="content">
+     <h1>{profile.name}</h1>
+     <h2>{profile.title}</h2>
+     <p class="bio">{profile.bio}</p>
+     <p class="skills"> {profile.skills}</p>
+     <p class="philosophy"> {profile.philosophy}</p>
+     <p class="cta"> {profile.cta}</p>
+
+
+     <div class="contact-links">
+      <a href="https://github.com/eterima" target="_blank">
+         <i class="fab fa-github"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/eterima/" target="_blank">
+         <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="mailto:e.terima@epilot.cloud">
+         <i class="fas fa-envelope"></i>
+      </a>
+   </div>
+   
+
+     <div class="buttons">
+        <button class="button" on:click={() => window.location.href = profile.portfolioLink}>Get to know epilot!</button>
+        <button class="button" on:click={() => window.location.href = profile.scheduleLink}>Schedule a Meeting</button>
+     </div>
+  </div>
+</div>
